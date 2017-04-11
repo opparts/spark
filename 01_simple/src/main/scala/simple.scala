8b52ce72295807e05fef2c796c0d7535b9fadba0
apple <apple@192.168.0.103>
2017-04-11 20:00:17 +0800

    no changes

--- 01_simple/src/main/scala/simple.scala
+++ 01_simple/src/main/scala/simple.scala
@@ -9,12 +9,9 @@ object simple {
     val logFile = "/Applications/spark-2.1.0-bin-hadoop2.7/README.md" // Should be some file on your system
     val conf = new SparkConf().setAppName("Simple Application")
     val sc = new SparkContext(conf)
-    val logData = sc.textFile(logFile, 2).cache()
-    val numAs = logData.filter(line => line.contains("a")).count()
-    val numBs = logData.filter(line => line.contains("b")).count()
-    println(s"Lines with a: $numAs, Lines with b: $numBs")
+    val logData = sc.textFile(logFile)
     val firstline = logData.first()
-    println(s"first line is firstline")
+    println(s"first line is $firstline")
     sc.stop()
   }
 }
