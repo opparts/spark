749f8db5f789e552d9b151f99ac3816ce9f8f1de
apple <apple@192.168.0.103>
2017-04-11 20:26:44 +0800

    增加了file的说明

--- 01_simple/src/main/scala/simple.scala
+++ 01_simple/src/main/scala/simple.scala
@@ -6,12 +6,14 @@ import org.apache.spark.SparkConf
 
 object simple {
   def main(args: Array[String]) {
-    val logFile = "/Applications/spark-2.1.0-bin-hadoop2.7/README.md" // Should be some file on your system
+    val logFile = "file:///Applications/spark-2.1.0-bin-hadoop2.7/README.md" // Should be some file on your system
     val conf = new SparkConf().setAppName("Simple Application")
     val sc = new SparkContext(conf)
     val logData = sc.textFile(logFile)
     val firstline = logData.first()
+    val count = logData.count();
     println(s"first line is $firstline")
+    println(s"total line is $count")
     sc.stop()
   }
 }
