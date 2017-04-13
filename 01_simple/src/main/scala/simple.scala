1c6b5da8bc4656b98445cf02639fa4140fd2e302
apple <apple@localhost>
2017-04-13 23:07:39 +0800

    02_01一起提交

--- 01_simple/src/main/scala/simple.scala
+++ 01_simple/src/main/scala/simple.scala
@@ -7,8 +7,11 @@ import org.apache.spark.SparkConf
 object simple {
   def main(args: Array[String]) {
     val logFile = "file:///Applications/spark-2.1.0-bin-hadoop2.7/README.md" // Should be some file on your system
-    val conf = new SparkConf().setAppName("Simple Application")
+    
+    val conf = new SparkConf().setAppName("Simple Application - cluster 6066 Mode")
     val sc = new SparkContext(conf)
+    
+    //创建一个名为logData的RDD对象
     val logData = sc.textFile(logFile)
     val firstline = logData.first()
     val count = logData.count();
