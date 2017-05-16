9e8af81a818a66fdcf749c4ab8070fc295c2ea96
opparts <ssole@163.com>
2017-05-16 21:29:44 +0800

    finished demo

--- 02_simple/build.sbt
+++ 02_simple/build.sbt
@@ -5,5 +5,9 @@ version := "1.0"
 scalaVersion := "2.11.8"
 
 
-// additional libraries]
- libraryDependencies += "org.apache.spark" %% "spark-core" % "2.1.0";
+libraryDependencies ++= Seq(
+  "org.apache.spark" %% "spark-core" % "2.1.0",
+  "org.apache.spark" %% "spark-sql"  % "2.1.0"
+)
+
+
--- 02_simple/src/main/scala/demo.scala
+++ 02_simple/src/main/scala/demo.scala
@@ -1,5 +1,6 @@
 package main.scala
 
+import org.apache.spark.sql.SQLContext
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkContext._
 import org.apache.spark.SparkConf
@@ -18,12 +19,23 @@ object demo {
 
     //创建一个名为logData的RDD对象
     val user = sc.textFile(path);
+    
+    println("-------Using textFile去读取CSV成为RDD，然后OpenCSV Reader来读取文件-------");
     val result = user.map {
       line =>
         val reader = new CSVReader(new StringReader(line));
         reader.readNext();
     };
     result.collect().foreach( x => { x.foreach(println);println("--------------")});
+    
+    println("---------------------SparkSQL Load方式来读取CSV文件 -------------------");
+    val sqlcontext = new SQLContext(sc);
+    val df = sqlcontext.load("com.databricks.spark.csv", Map("path" -> "file:///Users/apple/user.csv", "header" -> "true"));
+    
+    val result2 = df.select("name");
+    result2.collect().foreach(println);
+  
+    
     sc.stop()
   }
 }
